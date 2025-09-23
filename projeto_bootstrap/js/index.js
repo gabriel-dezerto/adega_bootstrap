a8d23cf0ddaa5ed760f056319d5e6977606fbc36
gabriel-dezerto <dezertogabriel@gmail.com>
2025-09-23 09:47:56 -0300

    Criando e adicionando os scripts do site

---
+++ projeto_bootstrap/js/index.js
@@ -0,0 +1,12 @@
+// Mensagem dde adicionar no carrinho - Gin
+document.getElementById("btnCarrinho").addEventListener('click', function(){
+    document.getElementById("mensagemCarrinho").style.display = "block";
+});
+// Mensagem dde adicionar no carrinho - Vodka
+document.getElementById("btnCarrinho2").addEventListener('click', function(){
+    document.getElementById("mensagemCarrinho2").style.display = "block";
+});
+// Mensagem dde adicionar no carrinho - Whisky
+document.getElementById("btnCarrinho3").addEventListener('click', function(){
+    document.getElementById("mensagemCarrinho3").style.display = "block";
+});
